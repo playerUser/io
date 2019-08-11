9387dddd3c6990ac0e592893d4f2b91dba53dc77
playerUser <51266432+playerUser@users.noreply.github.com>
2019-08-11 17:12:06 +0800

    完善client端

--- timeserver/nio/TimeClient.java
+++ timeserver/nio/TimeClient.java
@@ -5,6 +5,11 @@ package io.timeserver.nio;
  */
 public class TimeClient {
 
+    public static void main(String[] args) {
+
+        int port = 8080;
+        new Thread(new TimeClientHandler("127.0.0.1", port), "TimeClient-001").start();
+    }
 
 
 }
