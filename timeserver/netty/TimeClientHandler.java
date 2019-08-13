5210aef4760726a35489f93b830c24ab464056ea
playerUser <51266432+playerUser@users.noreply.github.com>
2019-08-13 19:28:08 +0800

    新增netty timeserver代码

---
+++ timeserver/netty/TimeClientHandler.java
@@ -0,0 +1,45 @@
+package io.timeserver.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.logging.Logger;
+
+/**
+ * User: aaa
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter {
+
+    private static final Logger LOGGER = Logger.getLogger(TimeClientHandler.class.getName());
+
+    private final ByteBuf firstMessage;
+
+    public TimeClientHandler() {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is: " + body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOGGER.warning("Unexpected exception from downstream: " + cause.getMessage());
+        ctx.close();
+    }
+}
